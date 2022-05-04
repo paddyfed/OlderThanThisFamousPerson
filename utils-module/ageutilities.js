91de693cffe2f039343f1bdc15ac6f6915aef217
Paddy <paddyfedoo@hotmail.com>
2022-05-04 20:47:39 +0100

    Refactoring the find Celebrity functions

--- utils-module/ageutilities.js
+++ utils-module/ageutilities.js
@@ -1,64 +1,61 @@
-exports.findSameAgeCelebrities = function (userAgeInDays, listOfCelebrities, numberOfMatches, daysOlderThan = 30) {
-    let returnedCelebrities = []
-    let i = 0
-
-    listOfCelebrities.sort(function (a, b) {
-        return b.ageInDays - a.ageInDays
-    })
-    
-    listOfCelebrities.forEach(celebrity => {
-        if (celebrity.ageInDays == userAgeInDays) {
-            returnedCelebrities.push(celebrity)
-        }
-    })
-
-    if (numberOfMatches) {
-        // if we want to restrict the number of results then Slice 
-        // will return the first X number of results from the Celebrities array
-        return returnedCelebrities.slice(0, numberOfMatches)
-    }
-    else {
-        return returnedCelebrities
-    }
+exports.findSameAgeCelebrities = function (userAgeInDays, listOfCelebrities, numberOfMatches) {
+    return findCelebrities(userAgeInDays, listOfCelebrities, numberOfMatches, "SameAge")
 }
 
-exports.findOlderCelebrities = function (userAgeInDays, listOfCelebrities, numberOfMatches, daysOlderThan = 30) {
-    let returnedCelebrities = []
-    let i = 0
-
-    listOfCelebrities.sort(function (a, b) {
-        return a.ageInDays - b.ageInDays
-    })
-    
-    listOfCelebrities.forEach(celebrity => {
-        if (celebrity.ageInDays > userAgeInDays) {
-            returnedCelebrities.push(celebrity)
-        }
-    })
-
-    if (numberOfMatches) {
-        // if we want to restrict the number of results then Slice 
-        // will return the first X number of results from the Celebrities array
-        return returnedCelebrities.slice(0, numberOfMatches)
-    }
-    else {
-        return returnedCelebrities
-    }
+exports.findOlderCelebrities = function (userAgeInDays, listOfCelebrities, numberOfMatches) {
+    return findCelebrities(userAgeInDays, listOfCelebrities, numberOfMatches, "Older")
 }
 
-exports.findYoungerCelebrities = function (userAgeInDays, listOfCelebrities, numberOfMatches, daysOlderThan = 30) {
-    let returnedCelebrities = []
-    let i = 0
+exports.findYoungerCelebrities = function (userAgeInDays, listOfCelebrities, numberOfMatches) {
+    return findCelebrities(userAgeInDays, listOfCelebrities, numberOfMatches, "Younger")
+}
 
-    listOfCelebrities.sort(function (a, b) {
-        return b.ageInDays - a.ageInDays
-    })
-    
-    listOfCelebrities.forEach(celebrity => {
-        if (celebrity.ageInDays < userAgeInDays) {
-            returnedCelebrities.push(celebrity)
-        }
-    })
+function findCelebrities(userAgeInDays, listOfCelebrities, numberOfMatches, searchOption) {
+    let returnedCelebrities = []
+
+    const younger = () => {
+        listOfCelebrities.sort(function (a, b) {
+            return b.ageInDays - a.ageInDays
+        })
+
+        listOfCelebrities.forEach(celebrity => {
+            if (celebrity.ageInDays < userAgeInDays) {
+                returnedCelebrities.push(celebrity)
+            }
+        })
+    }
+
+    const older = () => {
+        listOfCelebrities.sort(function (a, b) {
+            return a.ageInDays - b.ageInDays
+        })
+
+        listOfCelebrities.forEach(celebrity => {
+            if (celebrity.ageInDays > userAgeInDays) {
+                returnedCelebrities.push(celebrity)
+            }
+        })
+    }
+
+    const sameAge = () => {
+        listOfCelebrities.sort(function (a, b) {
+            return b.ageInDays - a.ageInDays
+        })
+
+        listOfCelebrities.forEach(celebrity => {
+            if (celebrity.ageInDays == userAgeInDays) {
+                returnedCelebrities.push(celebrity)
+            }
+        })
+    }
+
+    const handleOption = {
+        'Younger': younger,
+        'Older': older,
+        'SameAge': sameAge
+    }
+
+    handleOption[searchOption]()
 
     if (numberOfMatches) {
         // if we want to restrict the number of results then Slice 
